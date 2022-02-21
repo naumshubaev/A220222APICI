9092854c02f9b99159806b2d63cf3bd1cc32aa4b
naumshubaev <90708953+naumshubaev@users.noreply.github.com>
2022-02-21 22:01:07 +0300

    TestV6 .body(...)

--- src/test/java/ru/netology/rest/MobileBankApiTestV6.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV6.java
@@ -29,6 +29,8 @@ class MobileBankApiTestV6 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200);
+          .statusCode(200)
+          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+          ;
     }
 }
