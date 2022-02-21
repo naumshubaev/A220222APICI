03777871b9fcbe87db6d000ecaecd9a11f112f34
naumshubaev <90708953+naumshubaev@users.noreply.github.com>
2022-02-21 22:07:52 +0300

    TestV6 JsonSchemaValidator added

--- src/test/java/ru/netology/rest/MobileBankApiTestV6.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV6.java
@@ -7,6 +7,7 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV6 {
     private RequestSpecification requestSpec = new RequestSpecBuilder()
